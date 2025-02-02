cc8cd29b8eacff5193d4dec0a83c964f151536d3
Gezer <gezer@inventotecnologia.com.br>
2025-02-01 22:25:48 -0300

    testing

--- src/api/controllers/manager.controller.js
+++ src/api/controllers/manager.controller.js
@@ -10,7 +10,13 @@ exports.instanceIndex = async (req, res) => {
   let InstanceInfo = await InstancesModel.findOne({ key: key });
 
   if (!InstanceInfo) {
-    return res.render('manager/instance/edit-Instance', { key: key, InstanceInfo: false });
+    return res.render('manager/instance/edit-Instance',
+      {
+        key: key,
+        InstanceInfo: false,
+        menu: "instance"
+      }
+    );
   }
   const db = mongoClient.db(config.mongoose.dbName);
   const collections = await db.listCollections({ name: key }).toArray();
@@ -40,5 +46,108 @@ exports.instanceIndex = async (req, res) => {
   // verifica se existe uma sessão anterior
   instanceWp['instance_session_previous'] = exists
 
-  return res.render('manager/instance/edit-Instance', { key: req.params.key, InstanceInfo: instanceWp });
+  return res.render('manager/instance/edit-Instance',
+    {
+      key: req.params.key,
+      InstanceInfo: instanceWp,
+      menu: "instance"
+    }
+  );
+}
+
+//WebSocke
+exports.instanceEventsWebSocketIndex = async (req, res) => {
+
+  let key = req.params.key;
+  let InstanceInfo = await InstancesModel.findOne({ key: key });
+
+  if (!InstanceInfo) {
+    return res.render('manager/instance/events/web-socket/edit-events-web-socket', {
+      key: key,
+      InstanceInfo: false,
+      menu: "web-socket"
+    });
+  }
+  const db = mongoClient.db(config.mongoose.dbName);
+  const collections = await db.listCollections({ name: key }).toArray();
+  const exists = collections.length > 0;
+  // total de grupos e total de contatos
+  let groupCount = await GroupsModel.countDocuments({ instance: key });
+  let contactCount = await ContactsModel.countDocuments({ instance: key });
+
+
+  let instanceWp = {
+    name: InstanceInfo.name,
+    key: InstanceInfo.key,
+    token: InstanceInfo.token,
+    phoneIdConnected: InstanceInfo.phoneIdConnected,
+    createdAt: InstanceInfo.createdAt,
+    dashboard: {
+      groupCount: groupCount,
+      contactCount: contactCount,
+      messagesSendCount: InstanceInfo.messagesSendCount,
+    },
+  };
+  if (WhatsAppInstances[InstanceInfo.key]) {
+    instanceWp['instance_session'] = await WhatsAppInstances[InstanceInfo.key].getInstanceDetail(InstanceInfo.key)
+  } else {
+    instanceWp['instance_session'] = null
+  }
+  // verifica se existe uma sessão anterior
+  instanceWp['instance_session_previous'] = exists
+
+  return res.render('manager/instance/events/web-socket/edit-events-web-socket', {
+    key: req.params.key,
+    InstanceInfo: instanceWp,
+    menu: "web-socket"
+  });
+}
+
+//webhook
+exports.instanceEventsWebhookIndex = async (req, res) => {
+
+  let key = req.params.key;
+  let InstanceInfo = await InstancesModel.findOne({ key: key });
+
+  if (!InstanceInfo) {
+    return res.render('manager/instance/events/webhook/edit-events-webhook', {
+      key: key,
+      InstanceInfo: false,
+      menu: "webhook"
+    });
+  }
+  const db = mongoClient.db(config.mongoose.dbName);
+  const collections = await db.listCollections({ name: key }).toArray();
+  const exists = collections.length > 0;
+  // total de grupos e total de contatos
+  let groupCount = await GroupsModel.countDocuments({ instance: key });
+  let contactCount = await ContactsModel.countDocuments({ instance: key });
+
+
+  let instanceWp = {
+    name: InstanceInfo.name,
+    key: InstanceInfo.key,
+    token: InstanceInfo.token,
+    phoneIdConnected: InstanceInfo.phoneIdConnected,
+    createdAt: InstanceInfo.createdAt,
+    dashboard: {
+      groupCount: groupCount,
+      contactCount: contactCount,
+      messagesSendCount: InstanceInfo.messagesSendCount,
+    },
+  };
+  if (WhatsAppInstances[InstanceInfo.key]) {
+    instanceWp['instance_session'] = await WhatsAppInstances[InstanceInfo.key].getInstanceDetail(InstanceInfo.key)
+  } else {
+    instanceWp['instance_session'] = null
+  }
+  // verifica se existe uma sessão anterior
+  instanceWp['instance_session_previous'] = exists
+
+  return res.render('manager/instance/events/webhook/edit-events-webhook', {
+    key: req.params.key,
+    InstanceInfo: instanceWp,
+    menu: "webhook"
+
+  });
 }
--- src/api/routes/manager.route.js
+++ src/api/routes/manager.route.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const { checkToken } = require("../middlewares/ChechToken");
 const router = express.Router()
-const { instanceIndex } = require('../controllers/manager.controller')
-// Rota para exibir a página de login
+const { instanceIndex, instanceEventsWebhookIndex, instanceEventsWebSocketIndex } = require('../controllers/manager.controller')
+
 router.route('/').get(checkToken, (req, res) => {
   const key = req.query.key || null; // Ou obtenha o valor de onde for necessário
   res.render('manager/dashboard', { error: req.flash('error'), success: req.flash('success'), key: key });
 });
 
 router.route('/instance/:key').get(checkToken, instanceIndex);
+
+router.route('/instance/:key/events/webhook').get(checkToken, instanceEventsWebhookIndex);
+
+//web-socket
+router.route('/instance/:key/events/web-socket').get(checkToken, instanceEventsWebSocketIndex);
+
 
 module.exports = router
